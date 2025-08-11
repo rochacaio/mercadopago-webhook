407e0a18a400720c9377de549e84075dee5a82b7
rochaacaio <caiorocha99@hotmail.com>
2025-08-11 14:11:29 -0300

    Remover arquivos sensíveis e configurar .gitignore

--- src/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-
-export const pool = new Pool({
-    user: process.env.DB_USER || "mpuser",
-    host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_NAME || "mpdb",
-    password: process.env.DB_PASS || "mpsenha",
-    port: Number(process.env.DB_PORT) || 5432,
-});
